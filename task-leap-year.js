1edaa3ed6d5103b18aaf53a6335e574172677378
Krzysztof Dąbrowski <chris@dabrowskisoftware.com>
2017-03-20 20:51:21 +0100

    one liner version of the leap.

--- task-leap-year.js
+++ task-leap-year.js
@@ -3,18 +3,8 @@
 // Lata przestępne to takie, których numer dzieli się przez 4, ale nie dzieli się przez 100, chyba
 // że dzieli się również przez 400.
 
-function isLeapYear(year) {
-    if (year % 400 === 0) {
-        return true;
-    }
+const isLeapYear = year => year % 400 === 0 || (year % 4 === 0 && year % 100 > 0);
 
-    if (year % 4 !== 0) {
-        return false;
-    }
-
-    return year % 100 !== 0;
-
-}
 console.log(isLeapYear(2000)); // true
 console.log(isLeapYear(2100)); // false
 console.log(isLeapYear(1996)); // true
