e8e8d60f7413b8ce2890be3f59834a352e0b5821
Przemyslaw Zabielski <przemyslaw.zabielski@gmail.com>
2017-03-22 12:18:51 +0100

    Added parameter

--- task-random-item.js
+++ task-random-item.js
@@ -1,8 +1,8 @@
 //3. Napisz funkcję randomItem(), która zwróci losowy element podanej tablicy.
 
+var array = [1, 2, 3, 4, 5, 6];
 
-function randomItem() {
-    var array = [1, 2, 3, 4, 5, 6];
-  return array[Math.floor(Math.random()*array.length)];
+function randomItem(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
-console.log(randomItem());
+console.log(randomItem(array));
