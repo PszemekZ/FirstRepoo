ee4a4a1e856d32716c547cbc968756a3cd867b36
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-22 16:32:01 +0100

    Revert "Refactoring"
    This reverts commit 35559175c3e0c078e32ff1e380956955067b3495.

--- task-random-item.js
+++ task-random-item.js
@@ -1,8 +1,11 @@
 //3. Napisz funkcję randomItem(), która zwróci losowy element podanej tablicy.
 
 
-function randomItem() {
-    var array = [1, 2, 3, 4, 5, 6];
-  return array[Math.floor(Math.random()*array.length)];
+function randomItem(randomItem){
+var array = [1, 2, 3, 4, 5, 6];
+
+var randomItem = array[Math.floor(Math.random()*array.length)];
+
+console.log(randomItem);
 }
 console.log(randomItem());
