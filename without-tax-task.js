e018487c7484d5161005d8925f68b8cbee09d82d
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-23 14:33:49 +0100

    Changing Math.floor on the round also added a const.

--- without-tax-task.js
+++ without-tax-task.js
@@ -1,9 +1,10 @@
 /*Napisz funkcję withoutTax(amount), która zwróci kwotę wynagrodzenia po odjęciu
 podatku dochodowego w wysokości 18 % .Wynik zaokrąglij do pełnych złotych.*/
 
+const taxPercent = 0.82;
 
 function withoutTax(amount) {
-    return Math.floor(amount * 0.82);
+    return Math.round(amount * taxPercent);
 };
 console.log(withoutTax(500));
 console.log(withoutTax(1545));
