bf448b9b071506905ef56bc89a5aaa8c4b37ec22
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-23 19:10:19 +0100

    refactoring

--- circle-area-task.js
+++ circle-area-task.js
@@ -1,7 +1,7 @@
 // 4. Napisz funkcję circleArea(diameter), która obliczy pole koła dla podanej średnicy. Użyj potęgowania.
 
 function circleArea(diameter) {
-    return Math.PI * Math.pow(diameter, 2);
+    return Math.PI * Math.pow(diameter / 2, 2);
 }
 console.log(circleArea(2));
 console.log(circleArea(3));
