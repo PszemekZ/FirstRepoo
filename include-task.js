36a64016b1b5166ee32996e9b23d07b376dfbc49
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-23 15:16:37 +0100

    I replaced function.

--- include-task.js
+++ include-task.js
@@ -1,14 +1,7 @@
 // 7. Napisz funkcję includes(a, b), która sprawdza, czy a zawiera się w b.
 
-var a = [1, 2, 3];
-
 function includes(a, b) {
-    for (var i = 0; i < a.length; i++) {
-        if (a[i] === b) {
-            return true;
-        }
-    }
-    return false;
+    return a.includes(b);
 }
-console.log(includes(a, 2)); //true
-console.log(includes(a, 5)); //false
+console.log(includes('Hello World', 'Hell')); //true
+console.log(includes('Hello World', 'dupa')); //false
