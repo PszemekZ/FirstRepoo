a1fd1eb099e17396b792fa53e1e13231f926383c
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-23 19:30:40 +0100

    refactoring

--- without-tax-task.js
+++ without-tax-task.js
@@ -4,8 +4,8 @@ podatku dochodowego w wysokości 18 % .Wynik zaokrąglij do pełnych złotych.*/
 const TAX_PERCENT = 18;
 
 function withoutTax(amount) {
-    return Math.round(amount - (TAX_PERCENT / 100) * amount);
-};
+    return Math.round(amount * (1 - TAX_PERCENT / 100));
+}
 console.log(withoutTax(500));
 console.log(withoutTax(2200));
 console.log(withoutTax(13531));
