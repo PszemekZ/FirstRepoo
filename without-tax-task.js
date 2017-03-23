ce162bdb0b49cb27fdd95012cbee724237a1b6bf
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-23 17:43:16 +0100

    If it's stupid but it works, it isn't stupid. : D

--- without-tax-task.js
+++ without-tax-task.js
@@ -1,11 +1,11 @@
 /*Napisz funkcję withoutTax(amount), która zwróci kwotę wynagrodzenia po odjęciu
 podatku dochodowego w wysokości 18 % .Wynik zaokrąglij do pełnych złotych.*/
 
-const taxPercent = 0.82;
+const TAX_PERCENT = 18;
 
 function withoutTax(amount) {
-    return Math.round(amount * taxPercent);
+    return Math.round(Math.abs(amount / 100 * TAX_PERCENT - amount));
 };
 console.log(withoutTax(500));
-console.log(withoutTax(1545));
+console.log(withoutTax(2200));
 console.log(withoutTax(13531));
