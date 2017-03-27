8c4475145f3e3a9391ade9d7b72f4d41c81bb291
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-27 16:28:27 +0200

    refactoring

--- random-strings.js
+++ random-strings.js
@@ -1,14 +1,15 @@
 // Napisz funkcję randomString(), która zwróci losowy łańcuch znaków o podanej długości.
 
-function randomString() {
-    var textLength = 7;
-    var text = "";
+var textLength;
+
+function randomString(textLength) {
+    var text = ""
     var array = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (var i = 0; i < textLength; i++) {
         text += array.charAt(Math.floor(Math.random() * array.length));
     }
     return text;
 }
-console.log(randomString());
-console.log(randomString());
-console.log(randomString());
+console.log(randomString(7));
+console.log(randomString(10));
+console.log(randomString(15));
