d351612ac3ecd0395dbb2ae14305577c4da99f66
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-27 18:25:57 +0200

    Argument name Change

--- truncate.js
+++ truncate.js
@@ -1,6 +1,6 @@
 //Napisz funkcję truncate(), która skróci podany łańcuch do podanej liczby znaków.
 
-function truncate(array, length) {
-    return array.substring(0, length);
+function truncate(string, length) {
+    return string.substring(0, length);
 }
 console.log(truncate("prostezadaniepl", 6));
