fffa9f710f7f1fca57e91e1bb56f25f6046e3141
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-27 17:16:40 +0200

    changed varables names.

--- random-strings.js
+++ random-strings.js
@@ -1,12 +1,12 @@
 // Napisz funkcję randomString(), która zwróci losowy łańcuch znaków o podanej długości.
 
 function randomString(textLength) {
-    var text = ""
-    var charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var output = ""
+    var charsList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (var i = 0; i < textLength; i++) {
-        text += charList.charAt(Math.floor(Math.random() * charList.length));
+        output += charsList.charAt(Math.floor(Math.random() * charsList.length));
     }
-    return text;
+    return output;
 }
 console.log(randomString(7));
 console.log(randomString(10));
