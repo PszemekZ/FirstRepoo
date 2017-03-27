273fc7f8563b2ab9e2ade83413f140823e55b551
Krzysztof Dąbrowski <chris@dabrowskisoftware.com>
2017-03-27 20:09:41 +0200

    added new function, randomBetween

--- task-random-item.js
+++ task-random-item.js
@@ -1,7 +1,13 @@
 //3. Napisz funkcję randomItem(), która zwróci losowy element podanej tablicy.
 
+function randomBetween(min, max) {
+    const possibleNumbers = max - min + 1;
+    return Math.floor(Math.random() * possibleNumbers + min);
+}
+
 function randomItem(array) {
-    return array[Math.floor(Math.random() * array.length)];
+    const index = randomBetween(0, array.length - 1);
+    return array[index];
 }
 
 var array = [1, 2, 3, 4, 5, 6];
@@ -11,5 +17,3 @@ var arrayThree = [113, 114, 115, 116, 117];
 console.log(randomItem(array));
 console.log(randomItem(arrayTwo));
 console.log(randomItem(arrayThree));
-
-//string example
