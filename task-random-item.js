cc4deba124b919c53e6ea098f47aad904dbfb2dc
Krzysztof Dąbrowski <chris@dabrowskisoftware.com>
2017-03-27 19:36:10 +0200

    more examples

--- task-random-item.js
+++ task-random-item.js
@@ -1,8 +1,15 @@
 //3. Napisz funkcję randomItem(), która zwróci losowy element podanej tablicy.
 
-var array = [1, 2, 3, 4, 5, 6];
-
 function randomItem(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
+
+var array = [1, 2, 3, 4, 5, 6];
+var arrayTwo = [17, 18, 19, 20, 21, 22];
+var arrayThree = [113, 114, 115, 116, 117];
+
 console.log(randomItem(array));
+console.log(randomItem(arrayTwo));
+console.log(randomItem(arrayThree));
+
+//string example
