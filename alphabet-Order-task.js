f191150a864f4b9b5f7e671989eec639be16382c
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-28 17:06:31 +0200

    Added space after //, new name of my parameter and remove 1 line of code

--- alphabet-Order-task.js
+++ alphabet-Order-task.js
@@ -2,10 +2,10 @@
 // uporządkowanymi według alfabetu („wyraz” - „arwyz”). Nie przejmuj się na razie polskimi
 // znakami :)
 
-function alphabetOrder(word) {
-    const newWord = word.split("").sort().join("");
-    return newWord;
+function alphabetOrder(elements) {
+    return newWord = elements.split("").sort().join("");
+
 }
 
-console.log(alphabetOrder("dupa")); //adpd
-console.log(alphabetOrder("placki ziemniaczane")); //aaacceeiiiklmnnpzz
+console.log(alphabetOrder("dupa")); // adpd
+console.log(alphabetOrder("placki ziemniaczane")); // aaacceeiiiklmnnpzz
