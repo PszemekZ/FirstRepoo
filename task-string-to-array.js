287ac4ddf182ef06abc64fcf406819f7209a38fd
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-29 10:51:05 +0200

    remove unnecessary loop

--- task-string-to-array.js
+++ task-string-to-array.js
@@ -1,13 +1,9 @@
 // Napisz funkcję stringToArray(), która skonwertuje łańcuch znaków na listę słów.
 
 function stringToArray(string) {
-    const words = string.split(' ');
+    return string.split('').join('');
 
-    for (let index = 0; index < words.length; index++) {
-        return words;
-
-    }
 }
 
-console.log(stringToArray("placki ziemniaczane z mielonym"));
+console.log(stringToArray("skonwertuje łańcuch znaków na listę słów."));
 console.log(stringToArray("nieoczywiste związki pokoleniowego wstydu i dumy"));
