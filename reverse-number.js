93248c1b399608b1100c2f0ddb4279b952f20a6d
Przemysław Zabielski <przemyslawzabielski@gmail.com>
2017-03-29 15:19:29 +0200

    changed function to the one liner version

--- reverse-number.js
+++ reverse-number.js
@@ -1,12 +1,10 @@
 // Napisz funkcję reverseNumber(), która odwróci podaną liczbę (1234 => 4321)
 
 function reverseNumber(number) {
-    const newNumber = number.split("").reverse().join('');
-    return newNumber;
-
+    return parseInt(number.toString().split("").reverse().join(''));
 }
 
-console.log(reverseNumber("1234")); //4321
-console.log(reverseNumber("345")); //123
-console.log(reverseNumber("1532")); //2351
-console.log(reverseNumber("343")); //343
+console.log(reverseNumber(1234)); // 4321
+console.log(reverseNumber(345)); // 123
+console.log(reverseNumber(1532)); // 2351
+console.log(reverseNumber(343)); // 343
