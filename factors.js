b4d66bad762a38e251bb0295655e9aa908381f82
Przemyslaw Zabielski <przemyslawzabielski1@gmail.com>
2017-04-09 16:17:10 +0200

    optimal solution

--- factors.js
+++ factors.js
@@ -4,17 +4,21 @@
 function factors(number) {
     const factorsArray = [];
 
-    for (let factor = 1; factor <= number / 2; factor++) {
+    for (let factor = 1; factor <= Math.sqrt(number); factor++) {
         if (number % factor === 0) {
             factorsArray.push(factor);
+
+            if (factor !== Math.sqrt(number)) {
+                factorsArray.push(number / factor);
+            }
         }
     }
 
-    factorsArray.push(number);
-    
-    return factorsArray;
+    return factorsArray.sort(function(a, b) {
+        return a - b;
+    });
 }
 
 console.log(factors(50)); // 1, 2, 5, 10, 25, 50
 console.log(factors(651));
-console.log(factors(532));
+console.log(factors(25)); // 1, 5, 25
