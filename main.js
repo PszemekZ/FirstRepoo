2893b1a04b0b4ac0ad6681eca62d672def812abc
Przemyslaw Zabielski <przemyslawzabielski1@gmail.com>
2017-04-09 17:57:15 +0200

    bubble sort

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-
-function bubbleSort(arr){
-   var len = arr.length;
-   for (var i = len-1; i>=0; i--){
-     for(var j = 1; j<=i; j++){
-       if(arr[j-1]>arr[j]){
-           var temp = arr[j-1];
-           arr[j-1] = arr[j];
-           arr[j] = temp;
-        }
-     }
-   }
-   return arr;
+function swap(array, index1, index2) {
+    const temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
 }
-bubbleSort([7,5,2,4,3,9]);
-bubbleSort([9,7,5,4,3,1]);
+
+function bubbleSort(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(array, j, j + 1);
+            }
+        }
+    }
+
+    return array;
+}
+
+console.log(bubbleSort([7, 5, 2, 4, 3, 9]));
+console.log(bubbleSort([9, 7, 5, 4, 3, 1]));
